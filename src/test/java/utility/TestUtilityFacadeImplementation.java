e50c81fca1dbc65d7319d3fda3060ea35736fd64
ZhanXDD <elgor2k1@gmail.com>
2021-09-30 11:59:42 +0200

    tests added

---
+++ src/test/java/utility/TestUtilityFacadeImplementation.java
@@ -0,0 +1,52 @@
+package utility;
+
+
+
+import java.util.Date;
+import java.util.Vector;
+
+import configuration.ConfigXML;
+import domain.Event;
+import domain.Team;
+
+/**
+ * Utilities to access Data Base
+ * @author IS2
+ *
+ */
+public class TestUtilityFacadeImplementation {
+		private TestUtilityDataAccess dbManagerTest;
+ 	
+    
+	   public TestUtilityFacadeImplementation()  {			
+			System.out.println("Creating TestFacadeImplementation instance");
+			ConfigXML.getInstance();
+			dbManagerTest=new TestUtilityDataAccess(); 
+			dbManagerTest.close();
+		}
+		
+		 
+		public boolean removeEvent(Event ev) {
+			dbManagerTest.open();
+			boolean b=dbManagerTest.removeEvent(ev);
+			dbManagerTest.close();
+			return b;
+
+		}
+		
+		public Event addEventWithQuestion(Team a, Team b, Date d, String question, float qty) {
+			dbManagerTest.open();
+			Event o=dbManagerTest.addEventWithQuestion(a,b,d, question, qty);
+			dbManagerTest.close();
+			return o;
+
+		}
+		
+		public Vector<Event> getEvents(Date date)  {
+			dbManagerTest.open();
+			Vector<Event>  events=dbManagerTest.getEvents(date);
+			dbManagerTest.close();
+			return events;
+		}
+
+}
